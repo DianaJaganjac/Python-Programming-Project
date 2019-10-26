80fced6a3b77322cd3878b3ebd963aaac28101eb
DijanaJag <56833411+DijanaJag@users.noreply.github.com>
2019-10-26 14:49:44 +0100

    Update test_cities.py

--- test_cities.py
+++ test_cities.py
@@ -30,6 +30,23 @@ def test_swap_cities():
     '''add your tests'''
 
 def test_shift_cities():
+    
+   road_map3 = [("California",	"Sacramento",	38.555605,	-121.468926),
+("Colorado",	"Denver",	39.7391667,	-104.984167),
+("Connecticut",	"Hartford",	41.767,	-72.677),
+("Delaware",	"Dover",	39.161921,	-75.526755),
+("Florida",	"Tallahassee",	30.4518,	-84.27277)]
+
+assert shift_cities(road_map3) ==\ [("Florida",	"Tallahassee",	30.4518,	-84.27277),
+                                    ("California",	"Sacramento",	38.555605,	-121.468926),
+                    ("Colorado",	"Denver",	39.7391667,	-104.984167),
+                    ("Connecticut",	"Hartford",	41.767,	-72.677),
+                    ("Delaware",	"Dover",	39.161921,	-75.526755)]
+    
+    
+    
+    
+    
     '''add your tests'''
 
 
