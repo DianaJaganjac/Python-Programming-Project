355df6f6cfcaffbdb97fe1cd7b9362d2e8c2fa8e
pop-one-demo <pop.one.demo.account@dcs.bbk.ac.uk>
2019-12-26 14:55:57 +0100

    'new'

--- cities.py
+++ cities.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 26 10:13:53 2019
+
+@author: Diana Jaganjac
+"""
+
 def read_cities(file_name):
     """
     Read in the cities from the given `file_name`, and return 
@@ -9,14 +16,42 @@ def read_cities(file_name):
 
       Alabama -> Alaska -> Arizona -> ... -> Wyoming -> Alabama.
     """
-    pass
+    f = open(file_name)
+    lines = f.readlines()
+    result = []
+    for line in lines:
+        line = line.strip("\n")
+        ele = line.split("\t")
+        joinele = (ele[0]), (ele[1]), (ele[2]), (ele[3])
+        result.append(joinele)
+    result.append(result[0])
+    return (result)
+    f.close
+    
+z = read_cities(r"C:\Users\Diana Jaganjac\pop-one-project-djagan01\city-data.txt")
+#print(z)
   
 def print_cities(road_map):
     """
     Prints a list of cities, along with their locations. 
     Print only one or two digits after the decimal point.
     """
-    pass
+    f = open(road_map)
+    lines = f.readlines()
+    result = []
+    for line in lines:
+        line = line.strip("\n")
+        ele = line.split("\t")
+        rm = ((ele[1]), ((ele[2])), ((ele[3])))
+        result.append(rm)    
+    result.append(result[0])    
+
+    return result
+    f.close
+
+x = print_cities(r"C:\Users\Diana Jaganjac\pop-one-project-djagan01\city-data.txt")
+print(x)
+    
 
 def compute_total_distance(road_map):
     """
@@ -24,12 +59,31 @@ def compute_total_distance(road_map):
     the connections in the `road_map`. Remember that it's a cycle, so that 
     (for example) in the initial `road_map`, Wyoming connects to Alabama...
     """
-    return 97.58
-
+    import math
+    coords = []
+    for i in road_map:
+        coords.append(i[1])
+        coords.append(i[2])
+        
+    x = [float(r) for r in coords[0::2]]
+    y = [float(r) for r in coords[1::2]]
+    
+    xy = list(zip(x,y))
+    
+    dist = 0
+    
+    for r in range(len(xy)-1):
+        dist += math.sqrt((xy[r][0]-xy[r+1][0])**2 + (xy[r][1]-xy[r+1][1])**2)
+    
+    return dist
+    
+    
+d = compute_total_distance(x)
+#print(d)
 
 def swap_cities(road_map, index1, index2):
     """
-    Take the city at location `index` in the `road_map`, and the 
+    Take the city at location `index1` in the `road_map`, and the 
     city at location `index2`, swap their positions in the `road_map`, 
     compute the new total distance, and return the tuple 
 
@@ -38,17 +92,49 @@ def swap_cities(road_map, index1, index2):
     Allow for the possibility that `index1=index2`,
     and handle this case correctly.
     """
-    return [("Connecticut", "Hartford", 41.767, -72.677),("Colorado", "Denver", 39.7391667, -104.984167),("California", "Sacramento", 38.555605, -121.468926)]
+    first_ele = road_map.pop(index1)
+    sec_ele = road_map.pop(index2-1)
+    
+    road_map.insert(index1, sec_ele)
+    road_map.insert(index2, first_ele)
+    
+    import math
+    coords = []
+    for i in road_map:
+        coords.append(i[1])
+        coords.append(i[2])
+        
+    x = [float(r) for r in coords[0::2]]
+    y = [float(r) for r in coords[1::2]]
+    
+    xy = list(zip(x,y))
+    
+    dist = 0
+    
+    for r in range(len(xy)-1):
+        dist += math.sqrt((xy[r][0]-xy[r+1][0])**2 + (xy[r][1]-xy[r+1][1])**2 )
+    
+    return (road_map, dist)
+   
 
-
+n = swap_cities(x, 0, 0)
+#print(n)
+        
 def shift_cities(road_map):
     """
     For every index i in the `road_map`, the city at the position i moves
     to the position i+1. The city at the last position moves to the position
     0. Return the new road map. 
     """
-    return [("Florida","Tallahassee", 30.4518, -84.27277),("California", "Sacramento", 38.555605, -121.468926),("Delaware", "Dover", 39.161921, -75.526755)]
+    #road_map.insert(0, road_map.pop())
+    #return road_map
+    string[i % len(string)]
+    
+    print(last)
 
+g = shift_cities(x)
+print(g)
+    
 def find_best_cycle(road_map):
     """
     Using a combination of `swap_cities` and `shift_cities`, 
